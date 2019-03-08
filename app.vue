437667a4d780edec9d84c2ff5d65e70bfd5108bf
11-ZS <48337121+11-ZS@users.noreply.github.com>
2019-03-08 08:45:14 +0800

    first upload file

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+      <router-link to='login' class='btn btn-lg '>login</router-link>
+      <router-link to='register' class='btn btn-lg'>register</router-link>
+
+        <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+<style>
+.router-link-active{
+    /* line-height:80px; */
+    border-radius: 4px;
+    border:1px solid red;
+    background-color:rgb(140, 248, 130)
+}
+
+</style>
+
